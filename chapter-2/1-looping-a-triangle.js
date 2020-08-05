0de42fc0869dc7449da94301f481f32723c3f885
julio-nf <julio.novais@outlook.com>
2020-08-04 21:30:44 -0300

    chapter-5 solutions & node + eslint

--- chapter-2/1-looping-a-triangle.js
+++ chapter-2/1-looping-a-triangle.js
@@ -1,3 +1,22 @@
+/*
+Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following
+triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+It may be useful to know that you can find the length of a string by writing
+.length after it.
+*/
+
+//My code
 let triangle = "#";
 for (let counter = 1; counter <= 7; counter++) {
     console.log(triangle);
--- chapter-3/1-mininum.js
+++ chapter-3/1-mininum.js
@@ -1,3 +1,11 @@
+/*
+Minimum
+
+The previous chapter introduced the standard function Math.min that returns its
+smallest argument. We can build something like that now. Write a function min
+that takes two arguments and returns their minimum.
+*/
+
 //My code
 const min = (x,y) => Math.min(x,y);
 
