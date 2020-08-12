b6839b05ebdef7f6303c00d86ec2dd340ee56a1d
julio-nf <julio.novais@outlook.com>
2020-08-11 21:11:38 -0300

    added exercises header and chapter-6 solutions

---
+++ chapter-6/04-borrowing-method.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-prototype-builtins */
+/*
+Earlier in the chapter I mentioned that an object’s hasOwnProperty can be used
+as a more robust alternative to the in operator when you want to ignore the
+prototype’s properties. But what if your map needs to include the word
+"hasOwnProperty"? You won’t be able to call that method anymore because the
+object’s own property hides the method value.
+
+Can you think of a way to call hasOwnProperty on an object that has its own
+property by that name?
+*/
+
+//My code
+const hasOwnPropertySymbol = Symbol("hasOwnProperty");
+let map = {one: true, two: true, [hasOwnPropertySymbol]() { return true } };
+
+// Fix this call
+console.log(map.hasOwnProperty("one"));
+// → true
